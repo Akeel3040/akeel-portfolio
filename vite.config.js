bd6580c34c0b0422ebadc2bb2341133a0c3c960d
Mohammad Akeel <akeelkhan3499@gmail.com>
2025-10-29 11:44:37 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/akeel-portfolio/', // 👈 apne GitHub repo ka naam
 })
